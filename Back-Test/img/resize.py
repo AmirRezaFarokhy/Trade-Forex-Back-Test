dc92ffc66d9b380722305e93812f363aa6af3528
AmirReza Farokhy <113052872+AmirRezaFarokhy@users.noreply.github.com>
2023-05-07 17:07:11 +0330

    Delete

--- Back-Test/img/resize.py
+++
@@ -1,9 +0,0 @@
-import cv2 
-import os
-
-for i in range(1, 5):
-    ls_name = f"test_{i}/"
-    for img in os.listdir(ls_name):
-        m = cv2.imread(f"{ls_name}{img}")
-        m = cv2.resize(m, (720, 480))
-        cv2.imwrite(f"{img}", m)
